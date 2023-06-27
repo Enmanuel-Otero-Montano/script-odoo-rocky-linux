9c7f936256101f72836a1652c3a9d985037749a8
Enmanuel-Otero-Montano <eotero@kreilabs.com>
2023-06-27 14:53:26 -0300

    Creo usuario odoo

--- odoo_install_rocky_linux.sh
+++ odoo_install_rocky_linux.sh
@@ -11,9 +11,10 @@ echo Updating software package
 sudo dnf update -y
 echo Installing Odoo Prerequsites
 sudo dnf install -y wget tar gcc git libpq-devel python-devel openldap-devel
+sudo dnf install -y postgresql-server
 sudo postgresql-setup --initdb --unit postgresql
 sudo systemctl enable --now postgresql
-sudo su -c "createuser -s $USER" postgres
+sudo su - postgres -c "createuser -s $USER" 
 #--------------------------------------------------------------------------------------------------------------------
 echo Installing WKHTMLTOX
 cd /tmp
@@ -21,7 +22,7 @@ wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-2/wkhtm
 sudo dnf localinstall -y wkhtmltox-0.12.6.1-2.almalinux9.x86_64.rpm
 #--------------------------------------------------------------------------------------------------------------------
 echo Installing Odoo
-#useradd -r -m -U -s /bin/bash -d /opt/odoo odoo
+sudo useradd -r -m -U -s /bin/bash -d /opt/odoo odoo
 #su - odoo
 sudo git clone https://www.github.com/odoo/odoo --depth 1 --branch $OE_VERSION /home/$USER/odoo
 #--------------------------------------------------------------------------------------------------------------------
@@ -84,3 +85,5 @@ EOF
 
 #Enable and start Odoo service
 sudo systemctl enable --now odoo.service
+cd /home/$USER/odoo
+./odoo-bin
