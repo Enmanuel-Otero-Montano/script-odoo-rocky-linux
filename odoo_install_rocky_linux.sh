27cee72fc4b2af08a3104d0d786ed3e272398faa
Enmanuel-Otero-Montano <eotero@kreilabs.com>
2023-06-27 15:54:21 -0300

    -

--- odoo_install_rocky_linux.sh
+++ odoo_install_rocky_linux.sh
@@ -3,6 +3,7 @@
 # Script for installing Odoo on Rocky Lunix 9.
 # Author: Enmanuel Otero Montano
 #--------------------------------------------------------------------------------------------------------------------
+OE_USER="odoo"
 OE_VERSION="16.0"
 OE_SUPERADMIN="admin"
 echo Starting the installation
@@ -10,11 +11,11 @@ sleep 2
 echo Updating software package
 sudo dnf update -y
 echo Installing Odoo Prerequsites
-sudo dnf install -y wget tar gcc git libpq-devel python-devel openldap-devel
+sudo dnf install -y wget tar gcc git libpq-devel python-devel python3-pip openldap-devel
 sudo dnf install -y postgresql-server
 sudo postgresql-setup --initdb --unit postgresql
 sudo systemctl enable --now postgresql
-sudo su - postgres -c "createuser -s $USER" 
+sudo su - postgres -c "createuser -s $OE_USER" 
 #--------------------------------------------------------------------------------------------------------------------
 echo Installing WKHTMLTOX
 cd /tmp
@@ -22,24 +23,22 @@ wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-2/wkhtm
 sudo dnf localinstall -y wkhtmltox-0.12.6.1-2.almalinux9.x86_64.rpm
 #--------------------------------------------------------------------------------------------------------------------
 echo Installing Odoo
-sudo useradd -r -m -U -s /bin/bash -d /home/$USER/odoo odoo
+sudo useradd -r -m -U -s /bin/bash -d /opt/odoo odoo
 #su - odoo
-sudo git clone https://www.github.com/odoo/odoo --depth 1 --branch $OE_VERSION /home/$USER/odoo
+sudo git clone https://www.github.com/odoo/odoo --depth 1 --branch $OE_VERSION /opt/odoo/odoo
 #--------------------------------------------------------------------------------------------------------------------
 echo Creating and activating a Python virtual environment for Odoo
-cd /home/$USER/odoo
-sudo chmod -R 777 /home/$USER/odoo
-python -m venv venv
-source /home/$USER/odoo/venv/bin/activate
+cd ~
+sudo python -m venv venv
+source venv/bin/activate
 #--------------------------------------------------------------------------------------------------------------------
 echo Upgrade pip "(Python Package Manager)"
 pip install --upgrade pip
-pip install -r /home/$USER/odoo/requirements.txt
+pip install -r /opt/odoo/odoo/requirements.txt
 deactivate
 #--------------------------------------------------------------------------------------------------------------------
 echo Configuring installation
-sudo mkdir /home/$USER/odoo/custom-addons
-sudo chmod -R 777 /home/$USER/odoo/custom-addons
+sudo mkdir /opt/odoo/custom-addons
 #Exit from odoo user shell
 #exit
 
@@ -49,18 +48,18 @@ sudo touch /var/log/odoo/odoo.log
 sudo chown -R odoo: /var/log/odoo/
 
 #Add following directives in file 'odoo.conf'
-sudo cat > /home/$USER/odoo/odoo.conf << EOF
+sudo cat > /etc/odoo.conf << EOF
 [options]
 This is the password that allows database operations:
 admin_passwd = $OE_SUPERADMIN
 db_host = False
 db_port = False
-db_user = $USER
+db_user = odoo
 db_password = False
 xmlrpc_port = 8069
 logfile = /var/log/odoo/odoo.log
 logrotate = True
-addons_path = /home/$USER/odoo/addons,/home/$USER/odoo/custom-addons
+addons_path = /opt/odoo/odoo/addons,/opt/odoo/custom-addons
 EOF
 
 #Create a Systemd Service Unit
@@ -76,7 +75,7 @@ SyslogIdentifier=odoo
 PermissionsStartOnly=true
 User=odoo
 Group=odoo
-ExecStart=/home/$USER/odoo/venv/bin/python3 /home/$USER/odoo/odoo-bin -c /home/$USER/odoo/odoo.conf
+ExecStart=/opt/odoo/venv/bin/python3 /opt/odoo/odoo/odoo-bin -c /etc/odoo.conf
 StandardOutput=journal+console
 
 [Install]
@@ -85,5 +84,3 @@ EOF
 
 #Enable and start Odoo service
 sudo systemctl enable --now odoo.service
-cd /home/$USER/odoo
-./odoo-bin
